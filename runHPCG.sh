da81802fc3e25b0136e66d8fbb379b58d16bf848
Jacob Liu <liu.jacob2004@gmail.com>
2023-07-27 19:46:26 +0000

    fixed script with relative paths

--- runHPCG.sh
+++ runHPCG.sh
@@ -1,10 +1,10 @@
 #! /bin/bash
 
 export NUM_PROCESSES=$1
-cd /home/user/hpcg/My_MPI_OpenMP
-/home/user/hpcg/configure My_MPI_OMP
+cd My_MPI_OpenMP
+../configure My_MPI_OMP
 make
-cd /home/user/hpcg/My_MPI_OpenMP/bin
+cd bin
 export OMP_NUM_THREADS=$2
 mpiexec -np $NUM_PROCESSES ./xhpcg
 
